8674e57ea1ab79748bd3f3bd1a9ad341e3569190
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:27:06 +0100

    Update clase.gd

--- clases/clase.gd
+++ clases/clase.gd
@@ -1,10 +1,10 @@
 
 extends Node
-class_name Persona #haciendo este script se pueden crear nodos de este tipo (no hace falta poner el script en escena)
-#se pueden agnadir hereditarios de este script como si fuesen un nodo mas de godot
-#tambien se pueden poner scripts de clase a un recurso vacio para almacenamiento de datos personalizados
+class_name Persona #Haciendo este script se pueden crear nodos de este tipo (no hace falta poner el script en escena)
+#Se pueden agnadir hereditarios de este script como si fuesen un nodo mas de godot
+#Tambien se pueden poner scripts de clase a un recurso vacio para almacenamiento de datos personalizados
 
-@export var nombre:String #variable publica de clase
-var tamagno:int #variable privada de clase
-func funcion(propiedad): #las funciones se pueden usar directamente
+@export var nombre:String #Variable publica de clase
+var tamagno:int #Variable privada de clase
+func funcion(propiedad): #Las funciones se pueden usar directamente
 	pass
