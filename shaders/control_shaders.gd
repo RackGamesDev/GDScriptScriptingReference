fb38d6e97af57d9acca2ec934c52a9369a0c0b7b
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:22:29 +0100

    Update control_shaders.gd

--- shaders/control_shaders.gd
+++ shaders/control_shaders.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 func _ready():
-	material.set_shader_parameter("reemplazo", Color.RED) #si un shader tiene parametros se pueden configurar desde el material
+	material.set_shader_parameter("reemplazo", Color.RED) #Si un shader tiene parametros se pueden configurar desde el material
 	print(material.get_shader_parameter("tolerancia"))
-	RenderingServer.global_shader_parameter_set("shaderglobal", 30) #interactuando con los parametros globales de shaders
+	RenderingServer.global_shader_parameter_set("shaderglobal", 30) #Interactuando con los parametros globales de shaders
 
