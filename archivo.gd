77d28409bc3ee20f9aca25683ca69ae88a18b0ed
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:21:12 +0100

    Update archivo.gd

--- archivo.gd
+++ archivo.gd
@@ -2,12 +2,12 @@ extends Node
 
 const RUTA = "data.json"
 
-func guardar(data): #guardar una variable (preferiblemente una tupla) como json
+func guardar(data): #Guardar una variable (preferiblemente una tupla) como json
 	var file := FileAccess.open(RUTA, FileAccess.WRITE)
 	file.store_line(JSON.stringify(data))
 	file.close()
 	
-func cargar(): #devuelve una tupla a partir de un json
+func cargar(): #Devuelve una tupla a partir de un json
 	if not FileAccess.file_exists(RUTA):
 		return
 	var file := FileAccess.open(RUTA, FileAccess.READ)
@@ -20,7 +20,7 @@ func cargar(): #devuelve una tupla a partir de un json
 	var data = JSON.parse_string(final)
 	return data
 	
-func cargar_texto(): #carga el texto de cualquier archivo de texto
+func cargar_texto(): #Carga el texto de cualquier archivo de texto
 	if not FileAccess.file_exists(RUTA):
 		return
 	var file := FileAccess.open(RUTA, FileAccess.READ)
