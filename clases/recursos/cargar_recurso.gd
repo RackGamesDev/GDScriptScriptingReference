db03c34e48d1f3bee324893a3d43a88cde81d276
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:27:21 +0100

    Update cargar_recurso.gd

--- clases/recursos/cargar_recurso.gd
+++ clases/recursos/cargar_recurso.gd
@@ -1,9 +1,9 @@
 extends Node
 
 func _ready():
-	var recurso_personalizado:Persona = preload("res://clases/recursos/new_resource.tres") #cargar recurso personalizado que hereda de la clase Persona
-	print(recurso_personalizado.nombre) #accediendo a el (lectura)
+	var recurso_personalizado:Persona = preload("res://clases/recursos/new_resource.tres") #Cargar recurso personalizado que hereda de la clase Persona
+	print(recurso_personalizado.nombre) #Accediendo a el (lectura)
 	
 	var recurso_guardar = Persona.new()
-	ResourceSaver.save(recurso_guardar, "user://data.tres") #tambien se pueden guardar como archivo (se pueden modificar)
+	ResourceSaver.save(recurso_guardar, "user://data.tres") #Tambien se pueden guardar como archivo (se pueden modificar)
 
