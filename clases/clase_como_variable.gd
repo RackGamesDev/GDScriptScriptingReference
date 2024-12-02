33be6614f08a51a5d5bb20dde3d70e7649aa7f40
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:26:36 +0100

    Update clase_como_variable.gd

--- clases/clase_como_variable.gd
+++ clases/clase_como_variable.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	var una_persona = Persona.new() #habiendo creado el script de la clase se puede usar como variable
-	print(una_persona.nombre) #accediendo a sus propiedades y metodos
+	var una_persona = Persona.new() #Habiendo creado el script de la clase se puede usar como variable
+	print(una_persona.nombre) #Accediendo a sus propiedades y metodos
 	print(una_persona.tamagno)
 	una_persona.funcion(3)
