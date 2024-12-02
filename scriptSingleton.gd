71bc04e29316b27fa68f4c2336af91f819f2ea98
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 02:21:56 +0100

    Update scriptSingleton.gd

--- scriptSingleton.gd
+++ scriptSingleton.gd
@@ -1,3 +1,3 @@
 extends Node
-# este script esta cargado como autoload/singleton, se creara por fuera de la escena en todo momento del juego
+# Este script esta cargado como autoload/singleton, se creara por fuera de la escena en todo momento del juego
 @export var VARGLOBAL : int = 1
